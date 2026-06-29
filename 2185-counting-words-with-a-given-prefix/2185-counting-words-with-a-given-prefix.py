cc1e3c666df3ec57c4f58eb48629d3de8cb454f5
Alfiya A Tamboli <alfiyat391@gmail.com>
2026-06-29 13:19:30 +0530

    Time: 7 ms (1.84%), Space: 12.3 MB (64.06%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
@@ -7,15 +7,7 @@ class Solution(object):
         """
         count=0
         for c in words:
-            i=0
-            flag=1
-            if len(c)>=len(pref):
-                for i in range(len(pref)):
-                    if c[i]!=pref[i]:
-                        flag=0
-                        break
-            
-                if flag==1:
-                    count+=1
-            
+            if c>=len(pref) and c[:len(pref)]==pref:
+                count+=1
         return count
+        
