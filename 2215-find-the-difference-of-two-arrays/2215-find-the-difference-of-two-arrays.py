983546635606df29866cb17a07702c6eab61de50
Alfiya A Tamboli <alfiyat391@gmail.com>
2026-06-29 15:02:04 +0530

    Time: 606 ms (14.71%), Space: 12.6 MB (93.73%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        answer=[0,0]
+        d1=[]
+        d2=[]
+
+        for i in range(len(nums1)):
+            if nums1[i] not in nums2 and nums1[i] not in d1:
+                d1.append(nums1[i])
+        for i in range(len(nums2)):
+            if nums2[i] not in nums1 and nums2[i] not in d2:
+                d2.append(nums2[i])
+
+        answer[0]=d1
+        answer[1]=d2
+
+        return answer
