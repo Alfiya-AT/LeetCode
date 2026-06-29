76a15f689c332e1b14158c0154638821561dee6f
Alfiya A Tamboli <alfiyat391@gmail.com>
2026-06-29 14:08:36 +0530

    Time: 0 ms (100%), Space: 12.4 MB (16.38%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.py
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.py
@@ -5,12 +5,15 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: int
         """
-        if nums1==nums2:
-            return 0
-        nums1.sort()
-        nums2.sort()
-        diff=nums2[0]-nums1[0]
-        for i in range(1,len(nums1)):
-            if (nums2[i]-nums1[i])!=diff:
-                return 
-        return diff
+        # if nums1==nums2:
+        #     return 0
+        # nums1.sort()
+        # nums2.sort()
+        # diff=nums2[0]-nums1[0]
+        # for i in range(1,len(nums1)):
+        #     if (nums2[i]-nums1[i])!=diff:
+        #         return 
+        # return diff
+
+
+        return min(nums2)-min(nums1)
