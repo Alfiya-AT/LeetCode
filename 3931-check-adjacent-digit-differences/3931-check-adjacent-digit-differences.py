993d85b3eedbd27e049ede1379f7c24b6e8e42fb
Alfiya A Tamboli <alfiyat391@gmail.com>
2026-07-25 08:40:22 +0530

    Time: 5 ms (81.13%), Space: 12.4 MB (54.25%) - LeetHub

---
+++ 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isAdjacentDiffAtMostTwo(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        for i in range(1,len(s)):
+            if abs(int(s[i])-int(s[i-1]))>2:
+                return False
+        return True
