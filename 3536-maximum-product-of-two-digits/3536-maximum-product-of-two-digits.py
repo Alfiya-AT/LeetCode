9eb05e1ddafd7bbd46a456b4b126bca69b0e9349
Alfiya A Tamboli <alfiyat391@gmail.com>
2026-07-25 06:45:53 +0530

    Time: 0 ms (100%), Space: 12.5 MB (16.08%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -4,11 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n<=9:
-            return n
-        elif len(str(n))==2:
-            num=str(n)
-            return int(num[-1])*int(num[-2])
-        res=[int(x) for x in str(n)]
-        res.sort()
-        return res[-1]*res[-2]
+        # if n<=9:
+        #     return n
+        # elif len(str(n))==2:
+        #     num=str(n)
+        #     return int(num[-1])*int(num[-2])
+        # res=[int(x) for x in str(n)]
+        # res.sort()
+        # return res[-1]*res[-2]
+
+        m=sorted(str(n))
+        return int(m[-1])*int(m[-2])
