d6dc2f6a9e22703f8589964b747ba7d0a76011dd
Alfiya A Tamboli <alfiyat391@gmail.com>
2026-07-25 06:44:36 +0530

    Time: 3 ms (46.15%), Space: 12.5 MB (16.08%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxProduct(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n<=9:
+            return n
+        elif len(str(n))==2:
+            num=str(n)
+            return int(num[-1])*int(num[-2])
+        res=[int(x) for x in str(n)]
+        res.sort()
+        return res[-1]*res[-2]
