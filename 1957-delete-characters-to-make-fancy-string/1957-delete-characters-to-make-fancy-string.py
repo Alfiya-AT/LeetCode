8c46aa26b31e2af49a51a597c1042f1bae044042
Alfiya A Tamboli <alfiyat391@gmail.com>
2026-08-13 10:11:03 +0530

    Time: 430 ms (91.67%), Space: 14.7 MB (45.24%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
@@ -4,15 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        if len(s)<=2:
-            return s
+
+        l=[]
+        for ch in s:
+            if len(l)<2 or ch!=l[-1] or ch!=l[-2]:
+                l.append(ch)
+
+        return "".join(l)
+
+        # if len(s)<=2:
+        #     return s
         
-        left=0
-        right=2
-        res=''
-        while right<len(s):
-            if s[left]!=s[left+1] or s[left]!=s[right]:
-                res+=s[left]
-            left+=1
-            right+=1
-        return res+s[-2:]
+        # left=0
+        # right=2
+        # res=''
+        # while right<len(s):
+        #     if s[left]!=s[left+1] or s[left]!=s[right]:
+        #         res+=s[left]
+        #     left+=1
+        #     right+=1
+        # return res+s[-2:]
