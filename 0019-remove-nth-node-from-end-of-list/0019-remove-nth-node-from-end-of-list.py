9ff2b50f913974ecdf2ba3e672106bd57653e7e6
Alfiya A Tamboli <alfiyat391@gmail.com>
2026-08-27 21:30:20 +0530

    Time: 0 ms (100%), Space: 12.3 MB (64.15%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -10,24 +10,47 @@ class Solution(object):
         :type n: int
         :rtype: Optional[ListNode]
         """
-        dummy = ListNode(0)
+        # dummy = ListNode(0)
+        # dummy.next=head
+
+        # length=0
+        # l=head
+        # while l:
+        #     length+=1
+        #     l=l.next
+        
+        # d=length-n
+        # prev=dummy
+        # curr=head
+
+        # i=0
+        # while i<d:
+        #     i+=1
+        #     curr=curr.next
+        #     prev=prev.next
+
+        # prev.next=curr.next
+        # return dummy.next
+
+        # using 2 ptr
+
+        dummy=ListNode(0)
         dummy.next=head
 
-        length=0
         l=head
-        while l:
-            length+=1
-            l=l.next
-        
-        d=length-n
-        prev=dummy
-        curr=head
+        jump=0
 
-        i=0
-        while i<d:
-            i+=1
+        while jump<n and l!=None:
+            l=l.next
+            jump+=1
+
+        prev=dummy
+        curr=l
+
+        while curr!=None:
             curr=curr.next
             prev=prev.next
 
-        prev.next=curr.next
+        prev.next=prev.next.next
+
         return dummy.next
