dedcaf0715ba54423373ff4ca4fce01a31a469f5
Alfiya A Tamboli <alfiyat391@gmail.com>
2026-09-05 09:39:54 +0530

    Time: 36 ms (58.29%), Space: 20.8 MB (85.03%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -4,9 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        setA=set()
-        for i in nums:
-            if i in setA:
-                return i
-            setA.add(i)
-        return setA
+        # setA=set()
+        # for i in nums:
+        #     if i in setA:
+        #         return i
+        #     setA.add(i)
+        # return setA
+
+
+        slow=0
+        fast=0
+        while True:
+            slow=nums[slow]
+            fast=nums[nums[fast]]
+
+            if slow==fast:
+                break
+
+        n1=0
+        n2=slow
+
+        while n1!=n2:
+            n1=nums[n1]
+            n2=nums[n2]
+
+        return n1
