534f908f47de75c6861a551caa4cb271ecf8d147
Alfiya A Tamboli <alfiyat391@gmail.com>
2026-09-14 21:16:27 +0530

    Time: 261 ms (64.9%), Space: 13.4 MB (68.77%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,25 +4,52 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if len(s)<=0:
-            return 0
-        elif len(s)==1:
-            return 1
-        elif len(s)==len(set(s)):
-            return len(s)
-        seen=set()
-        right=0
-        left=0
+
+
+        d={}
+        start=0
         maxLen=0
-        while right<len(s):
-            if s[right] in seen:
-                while left<right and s[left]!=s[right]:
-                    seen.remove(s[left])
-                    left+=1
-                left+=1
-            maxLen=max(maxLen,right-left+1)
-               
-            seen.add(s[right])
-            right+=1
+        for end,val in enumerate(s):
+            if val in d and d[val]>=start:
+                start=d[val]+1
+            d[val]=end
             
+            maxLen=max(maxLen,end-start+1)
+
+
         return maxLen
+
+
+
+
+
+
+
+
+
+        # if len(s)<=0:
+        #     return 0
+        # elif len(s)==1:
+        #     return 1
+        # elif len(s)==len(set(s)):
+        #     return len(s)
+        # seen=set()
+        # right=0
+        # left=0
+        # maxLen=0
+        # while right<len(s):
+        #     if s[right] in seen:
+        #         while left<right and s[left]!=s[right]:
+        #             seen.remove(s[left])
+        #             left+=1
+        #         left+=1
+        #     maxLen=max(maxLen,right-left+1)
+               
+        #     seen.add(s[right])
+        #     right+=1
+            
+        # return maxLen
+
+
+
+
