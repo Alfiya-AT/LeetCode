4dc6fb22148d1ce0fdaa9dedd534f2f0ff93288e
Alfiya A Tamboli <alfiyat391@gmail.com>
2026-09-15 11:21:50 +0530

    Time: 1874 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 select u.name,sum(t.amount) as balance
 from users as u 
-left join transactions as t 
+join transactions as t 
 on u.account = t.account
-group  by name
+group  by u.account,u.name
 having balance>10000;
